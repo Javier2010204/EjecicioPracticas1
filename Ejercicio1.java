23d98060b839baa2e4ed44131b954e2250b91c9c
Javier2010204 <2010204javiergomez@gmail.com>
2018-10-24 17:52:50 -0600

    First Commit

---
+++ Ejercicio1.java
@@ -0,0 +1,22 @@
+public class Ejercicio1{
+	public static void main (String args[]){
+		Integer numero1 = 100;
+		Integer numero2 = 50;
+
+
+
+		int suma = numero1 + numero2;
+		int resta = numero1 - numero2;
+		int multiplicacion = numero1 * numero2;
+		int division = numero1 / numero2;
+		int modulo = numero1%numero2;
+
+		System.out.println("La suma es: " + suma);
+		System.out.println("La resta es: " + resta);
+		System.out.println("La multiplicacion es: " + multiplicacion);
+		System.out.println("La division es: " + division);
+		System.out.println("El modulo es: " + modulo);
+
+
+	}
+}
